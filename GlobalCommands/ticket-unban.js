aec009402dc8cb0119acb5b182222fac660e1bec
Serverm636 <65414115+Serverm636@users.noreply.github.com>
2022-10-27 20:17:16 +0300

    first commit

---
+++ GlobalCommands/ticket-unban.js
@@ -0,0 +1,51 @@
+const { Client, CommandInteraction } = require('discord.js')
+const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
+const guildCommandsSchema = require('../Models/guildCommands-schema')
+
+module.exports = {
+    name: 'ticket',
+    description: 'generates the unban ticket',
+    options: [
+        {
+            name: 'unban',
+            type: 'SUB_COMMAND',
+            description: 'generates the unban ticket',
+        },
+    ],
+    async execute(client, interaction) {
+        if (interaction.member.permissions.has('ADMINISTRATOR')) {
+            let row = new MessageActionRow()
+                .addComponents(new MessageButton()
+                .setCustomId("open-ticket")
+                .setLabel("Open ticket")
+                .setEmoji("📩")
+                .setStyle("PRIMARY")
+            );
+            const mesaj = new MessageEmbed()
+                    .setTitle('Unban ticket')
+                    .setDescription('If you see this it means you are banned.\nTo open an unban ticket, please click on the button below.')
+                    .setColor('RED')
+                    
+            const guildId = interaction.guild.id
+            let schema = await guildCommandsSchema.findOne({
+                guildID: guildId,
+            })
+            if (!schema.bannedChannel) {
+                return await interaction.reply('**❌ You have not set up the banned channel. Please use `/set banned-channel`**')
+            }
+            const channel = schema.bannedChannel
+
+            if (!schema.bannedCategory) {
+                return await interaction.reply('**❌ You have not set up the banned category. Please use `/set banned-category`**')
+            }
+            
+            if (!schema.staffRole) {
+                return await interaction.reply('**❌ You have not set up the staff role. Please use `/set staff-role`**')
+            }
+
+            client.channels.cache.get(channel).send({ embeds: [mesaj], components: [row] });
+            return await interaction.reply({ content: '✅ Generated'} );
+        }
+        await interaction.reply({ content: '**❌ You are not authorized to use this**' });
+    }
+}
